9a682d3b49a3a62b818e837d9365dcbba9fd9f98
Vladimir Lopez <vladimir.luna96@hotmail.com>
2024-03-06 23:26:54 -0600

    Add nuevo registro en DB

--- src/main/java/com/vladimir/curso/springboot/jpa/springbootjpa/SpringbootJpaApplication.java
+++ src/main/java/com/vladimir/curso/springboot/jpa/springbootjpa/SpringbootJpaApplication.java
@@ -1,7 +1,7 @@
 package com.vladimir.curso.springboot.jpa.springbootjpa;
 
 import java.util.List;
-import java.util.Optional;
+//import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
